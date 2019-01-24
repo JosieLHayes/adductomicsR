a3e3946a6b964c7f454a8d0e950cfcf7c1f26165
Josie Hayes <jlhayes1982@gmail.com>
2019-01-23 18:51:49 -0800

    set nCores to NULL

--- R/specSimPepId.R
+++ R/specSimPepId.R
@@ -782,7 +782,7 @@ specSimPepId <-
                     resTable <-
                         resTable[resTable$meanSNRVar != 0,
                                  , drop = FALSE]
-                    return(resTable)
+                    
                 
                     }
             }
