e811588919c23f8d0a46d49a980c1584178605b5
Josie Hayes <jlhayes1982@gmail.com>
2019-01-27 17:36:34 -0800

    reformat indents, document S4 methods

--- R/IsotopicDistributionMod.R
+++ R/IsotopicDistributionMod.R
@@ -23,62 +23,62 @@ IsotopicDistributionMod <- function(formula = list(), charge = 1) {
         )
     inputFormula[names(formula)] <- formula
     simulation <- function(inputFormula) {
-        data = read.table(paste0(system.file("extdata", 
-        package = "adductomicsR"), '/ElementMasses.txt'), h=TRUE)
-        row.names(data)= data[,1]
-        data = data[,-1]
         massCarbon <- sum(sample(
-            as.numeric(data['C', seq(1, 2)]),
-            size = inputFormula$C,
+            c(12, 13.0033548378),
+            size =
+                inputFormula$C,
             replace = TRUE,
-            prob = as.numeric(data['C', seq(5, 6)])
+            prob = c(0.9893, 0.0107)
         ))
         massHydrogen <-
             sum(sample(
-                as.numeric(data['H', seq(1, 2)]),
+                c(1.0078250321, 2.014101778),
                 size = inputFormula$H,
                 replace = TRUE,
-                prob = as.numeric(data['H', seq(5, 6)])
+                prob = c(0.999885, 0.000115)
             ))
         massNitrogen <-
             sum(sample(
-                as.numeric(data['N', seq(1, 2)]),
+                c(14.0030740052, 15.0001088984),
                 size = inputFormula$N,
                 replace = TRUE,
-                prob = as.numeric(data['N', seq(5, 6)])
+                prob = c(0.99632, 0.00368)
             ))
         massOxygen <-
             sum(sample(
-                as.numeric(data['O', seq(1, 3)]),
+                c(15.9949146221, 16.9991315, 17.9991604),
                 size = inputFormula$O,
                 replace = TRUE,
-                prob = as.numeric(data['O', seq(5,7)])
+                prob = c(0.99757, 0.00038, 0.00205)
             ))
         massSulfer <- sum(sample(
-            as.numeric(data['S', seq(1, 4)]),
+            c(31.97207069, 32.9714585,
+              33.96786683, 35.96708088),
             size = inputFormula$S,
             replace = TRUE,
-            prob = as.numeric(data['S', seq(5, 8)])
+            prob = c(0.9493, 0.0076,
+                     0.0429, 2e-04)
         ))
-        massPhosphorus <- inputFormula$P * as.numeric(data['P', 1])
+        massPhosphorus <- inputFormula$P * 30.97376151
         massBromine <- sum(sample(
-            as.numeric(data['Br', seq(1, 2)]),
+            c(78.9183376, 80.916291),
             size = inputFormula$Br,
             replace = TRUE,
-            prob = as.numeric(data['Br', seq(5, 6)])
+            prob = c(0.5069, 0.4931)
         ))
         massChlorine <- sum(sample(
-            as.numeric(data['Cl', seq(1, 2)]),
+            c(34.96885271, 36.9659026),
             size = inputFormula$Cl,
             replace = TRUE,
-            prob = as.numeric(data['Cl', seq(5, 6)])
+            prob = c(0.7578, 0.2422)
         ))
-        massFluorine <- inputFormula$F * as.numeric(data['F', 1])
+        massFluorine <- inputFormula$F * 18.9984032
         massSilicon <- sum(sample(
-            as.numeric(data['Si', seq(1, 3)]),
+            c(27.9769265327, 28.97649472,
+              29.97377022),
             size = inputFormula$Si,
             replace = TRUE,
-            prob = as.numeric(data['Si', seq(5, 7)])
+            prob = c(0.922297, 0.046832, 0.030872)
         ))
         massMolecule <-
             sum(
@@ -110,8 +110,8 @@ IsotopicDistributionMod <- function(formula = list(), charge = 1) {
     spectrum <- spectrum[spectrum$intensity != 0,]
     spectrum$percent <-
         with(spectrum, round(intensity / max(intensity) *
-                            100,
-                            digits = 2))
+                                 100,
+                             digits = 2))
     row.names(spectrum) <- seq_len((nrow(spectrum)))
     return(spectrum)
 }
